1e0fa1c5483d3d76642b1f1221dfa14bdb413e9d
Taha Rauf <rauftaha757@gmail.com>
2025-07-01 11:37:25 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-const express = require("express");
-require("dotenv").config();
-const connectDB = require("./db");
-const app = require("./app");
-const PORT = process.env.PORT || 8000;
-connectDB()
-  .then(
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    }),
-  )
-  .catch((err) => {
-    console.log(`Error in connection: ${err}`);
-  });
+
