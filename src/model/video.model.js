3fe40cd8d1c235087a53057e8fc50ae8adf17768
Taha Rauf <rauftaha757@gmail.com>
2025-07-01 11:37:23 +0500

    Add video.model.js

---
+++ src/model/video.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const VideoSchema=new mongoose.Schema({
+
+videofile:{
+    type:String,
+    required:true,
+    trim:true
+},
+thumbnail:{
+    type:String,
+    required:true,
+    trim:true
+},
+owner:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Users"
+},
+title:{
+    type:String,
+    required:true,
+    trim:true
+},
+views:{
+    type:Number,
+    default:0,
+    min:0
+},
+description:{
+     type:String,
+    required:true,
+},
+isPublished:{
+    type:Boolean,
+    default:false
+},
+duration:{
+    type:Number,
+    default:0,
+    min:0
+},
+
+},{timestamps:true})
