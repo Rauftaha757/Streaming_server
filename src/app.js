d6ed7d63317f12c3408c5631de307ed34af32a35
Taha Rauf <rauftaha757@gmail.com>
2025-07-03 07:54:08 +0500

    add

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ const app=express()
 
 app.use(express.json({ limit : "16kb"}))
 app.use(express.urlencoded({extended:true, limit :"16kb"}))
+app.use(express.static("public"));
 
+
+
+// routes
+const userRouter = require("./routes/userRoutes.js")
+app.use("/api",userRouter)
 
 module.exports = app;
 
