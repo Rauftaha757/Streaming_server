66a193e7130c5bc3e694b98dcb0d6017eacc4785
Taha Rauf <rauftaha757@gmail.com>
2025-07-03 07:55:17 +0500

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-
+const { connect } = require("mongoose");
+require("dotenv").config();
+const connectDB = require("./db");
+const app = require("./app");
+const PORT = process.env.PORT || 8000;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch((err) => {
+  console.error("Failed to connect to DB:", err);
+});
