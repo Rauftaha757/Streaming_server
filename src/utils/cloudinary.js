2207fe74cfbe9b9d41c190d9fd8067a31075505e
Taha Rauf <rauftaha757@gmail.com>
2025-07-04 07:54:26 +0500

    cloudinary functionality

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,34 +1,32 @@
-const cloudinary = require('cloudinary').v2;
-const fs =require("fs")
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+const fs = require("fs");
+const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+const CLOUDINARY_API_SECRET = process.env.api_secret;
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
 });
 
+const uploadoncloud = async function (localfilepath) {
+  try {
+    if (!localfilepath) {
+      return null;
+    } else {
+      // uplod on clouudinary
+      const response = await cloudinary.uploader.upload(localfilepath, {
+        resource_type: "auto",
+      });
+      console.log("File uploaded on cloudinary", response.url);
+      fs.unlinkSync(localfilepath);
+      return response;
+    }
+  } catch (error) {
+    fs.unlinkSync(localfilepath);
+    return null;
+  }
+};
 
-const uploadoncloud= async function (localfilepath){
-try{
-
-if(!localfilepath){return null}
-else{
-// uplod on clouudinary
-const response = await cloudinary.uploader.upload(localfilepath,{
-    resource_type:"auto"
-})
-console.log("File uploaded on cloudinary", response.url)
-return response;
-}
-
-}
-catch(error){
-fs.unlinkSync(localfilepath)
-return null;
-}
-}
-
-
-
-
-
-module.exports = {cloudinary,uploadoncloud};
+module.exports = { cloudinary, uploadoncloud };
